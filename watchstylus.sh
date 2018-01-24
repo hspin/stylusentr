d5b4c0a7ce7c8893f209a1a9bf1625b25029a51e
GS Orion <me.gsbit@hsx5.com>
2018-01-24 14:58:56 -0500

    updating watch scipt

--- watchstylus.sh
+++ watchstylus.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-ls ./out/*.css | entr tmux load-buffer -b tmp-copy-buffer ./out/styles.css
+if [[ "$TERM" =~ "screen".* ]]; then
+  echo "Inside tmux"
+  echo "starting..."
+else
+  echo "We are not in TMUX!"
+  # Launches tmux in a session called 'base'.
+  # tmux attach -t base || tmux new -s base
+  echo "exiting"
+  exit
+fi
+
+ls out/*.css | entr tmux load-buffer -b tmp-copy-buffer out/styles.css
